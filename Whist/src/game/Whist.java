e1a353fa6afa3ee338dc4632ed9e6598d73eb806
UnimelbAsterio <ylin4@student.unimelb.edu.au>
2020-10-25 19:08:28 +1100

    Properties Configuration Capability implemented,4 interactive players implemented, player type detection implemented, random seed implemented

--- Whist/src/game/Whist.java
+++ Whist/src/game/Whist.java
@@ -6,11 +6,24 @@ import ch.aplu.jgamegrid.*;
 import java.awt.Color;
 import java.awt.Font;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+//import java.util.concurrent.ThreadLocalRandom;
+
+
+import java.io.FileReader; //new
+import java.io.IOException; //new
+
+
 
 @SuppressWarnings("serial")
 public class Whist extends CardGame {
-	
+
+	private static int Seed; //Fixed Seed Number （new）
+	private static int nbPlayers; //number of players （new）
+	private static int nbStartCards; //number of Stating Cards （new）
+	private static int winningScore; //Score Required to Win the game (new)
+	public static Random random = new Random(Seed); //Random Using Seed （new）
+	private static int[] PlayerType = new int[4]; //Player Type (new)
+
   public enum Suit
   {
     SPADES, HEARTS, DIAMONDS, CLUBS
@@ -25,7 +38,7 @@ public class Whist extends CardGame {
   
   final String trumpImage[] = {"bigspade.gif","bigheart.gif","bigdiamond.gif","bigclub.gif"};
 
-  static final Random random = ThreadLocalRandom.current();
+  //static final Random random = ThreadLocalRandom.current(); //Random（Original）
   
   // return random Enum value
   public static <T extends Enum<?>> T randomEnum(Class<T> clazz){
@@ -48,11 +61,11 @@ public class Whist extends CardGame {
   public boolean rankGreater(Card card1, Card card2) {
 	  return card1.getRankId() < card2.getRankId(); // Warning: Reverse rank order of cards (see comment on enum)
   }
-	 
+
   private final String version = "1.0";
-  public final int nbPlayers = 4;
-  public final int nbStartCards = 13;
-  public final int winningScore = 24;
+  //public final int nbPlayers = 4; //number of players （Original）
+  //public final int nbStartCards = 13; //number of Stating Cards （Original）
+  //public final int winningScore = 24; //Score Required to Win the game (Original)
   private final int handWidth = 400;
   private final int trickWidth = 40;
   private final Deck deck = new Deck(Suit.values(), Rank.values(), "cover");
@@ -103,13 +116,29 @@ private void initRound() {
 		 hands = deck.dealingOut(nbPlayers, nbStartCards); // Last element of hands is leftover cards; these are ignored
 		 for (int i = 0; i < nbPlayers; i++) {
 			   hands[i].sort(Hand.SortType.SUITPRIORITY, true);
+
+			 	// Check If Player is Human (new)
+				if(PlayerType[i]==00){
+					// Set up human player for interaction (new)
+					CardListener cardListener = new CardAdapter()  // Human Player plays card
+					{
+						public void leftDoubleClicked(Card card) {
+							selected = card;
+							hands[0].setTouchEnabled(false);
+							hands[1].setTouchEnabled(false);
+							hands[2].setTouchEnabled(false);
+							hands[3].setTouchEnabled(false);
+						}
+					};
+					hands[i].addCardListener(cardListener);
+
+				}
+
 		 }
-		 // Set up human player for interaction
-		CardListener cardListener = new CardAdapter()  // Human Player plays card
-			    {
-			      public void leftDoubleClicked(Card card) { selected = card; hands[0].setTouchEnabled(false); }
-			    };
-		hands[0].addCardListener(cardListener);
+
+
+
+
 		 // graphics
 	    RowLayout[] layouts = new RowLayout[nbPlayers];
 	    for (int i = 0; i < nbPlayers; i++) {
@@ -149,9 +178,9 @@ private Optional<Integer> playRound() {  // Returns winner, if any
 	for (int i = 0; i < nbStartCards; i++) {
 		trick = new Hand(deck);
     	selected = null;
-        if (0 == nextPlayer) {  // Select lead depending on player type
-    		hands[0].setTouchEnabled(true);
-    		setStatus("Player 0 double-click on card to lead.");
+        if (PlayerType[nextPlayer]==00) {  // Select lead depending on player type (new)
+    		hands[nextPlayer].setTouchEnabled(true); // enable touch for this human player in this round (new)
+    		setStatus("Player "+nextPlayer+" double-click on card to lead."); //status (new)
     		while (null == selected) delay(100);
         } else {
     		setStatusText("Player " + nextPlayer + " thinking...");
@@ -173,9 +202,9 @@ private Optional<Integer> playRound() {  // Returns winner, if any
 		for (int j = 1; j < nbPlayers; j++) {
 			if (++nextPlayer >= nbPlayers) nextPlayer = 0;  // From last back to first
 			selected = null;
-	        if (0 == nextPlayer) {
-	    		hands[0].setTouchEnabled(true);
-	    		setStatus("Player 0 double-click on card to follow.");
+	        if (PlayerType[nextPlayer]==00) { // Select lead depending on player type (new)
+	    		hands[nextPlayer].setTouchEnabled(true); // enable touch for this human player in this round (new)
+	    		setStatus("Player "+nextPlayer+" double-click on card to follow."); //status (new)
 	    		while (null == selected) delay(100);
 	        } else {
 		        setStatusText("Player " + nextPlayer + " thinking...");
@@ -243,9 +272,77 @@ private Optional<Integer> playRound() {  // Returns winner, if any
     refresh();
   }
 
-  public static void main(String[] args)
-  {
-    new Whist();
+  public static void main(String[] args) throws IOException {
+
+	  /** Load properties for whist game based on either default or a properties file.**/
+	  Properties WhistProperties = setUpProperties();
+
+  	new Whist();
+
   }
 
+
+
+
+
+	//Allows Custom Properties (NEW)
+	static private Properties setUpProperties() throws IOException {
+		Properties WhistProperties = new Properties();
+
+		// Default properties
+		WhistProperties.setProperty("Seed", "30006");
+		WhistProperties.setProperty("nbPlayers", "4");
+		WhistProperties.setProperty("nbStartCards", "13");
+		WhistProperties.setProperty("winningScore", "24");
+		WhistProperties.setProperty("Player0Type", "00"); //Default 00 , (00 means its Human)
+		WhistProperties.setProperty("Player1Type", "01"); //Default 01 , (01 means No Filter + Random Selection)
+		WhistProperties.setProperty("Player2Type", "01"); //Default 01 , (01 means No Filter + Random Selection)
+		WhistProperties.setProperty("Player3Type", "01"); //Default 01 , (01 means No Filter + Random Selection)
+
+
+		// Read properties
+		FileReader inStream = null;
+		try {
+			inStream = new FileReader("whist.properties");
+			WhistProperties.load(inStream);
+		} finally {
+			if (inStream != null) {
+				inStream.close();
+			}
+		}
+
+		// Number of Players
+		Seed = Integer.parseInt(WhistProperties.getProperty("Seed"));
+		// Number of Players
+		nbPlayers = Integer.parseInt(WhistProperties.getProperty("nbPlayers"));
+		// Number of Starting Cards
+		nbStartCards = Integer.parseInt(WhistProperties.getProperty("nbStartCards"));
+		//Score Required to Win the game
+		winningScore = Integer.parseInt(WhistProperties.getProperty("winningScore"));
+		//Player 0 Type
+		PlayerType[0] = Integer.parseInt(WhistProperties.getProperty("Player0Type"));
+		//Player 1 Type
+		PlayerType[1] = Integer.parseInt(WhistProperties.getProperty("Player1Type"));
+		//Player 2 Type
+		PlayerType[2] = Integer.parseInt(WhistProperties.getProperty("Player2Type"));
+		//Player 3 Type
+		PlayerType[3] = Integer.parseInt(WhistProperties.getProperty("Player3Type"));
+
+
+
+		assert(nbPlayers > 1);
+		assert(nbStartCards > 1);
+
+		return WhistProperties;
+	}
+
+
+
+
+
+
+
 }
+
+
+
